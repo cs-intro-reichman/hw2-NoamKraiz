c4af9f4f540d29dcd94cba9d55fc36bd9a1bddc2
Noam Kraiz <noamkraiz@MacBook-Air-sl-Noam.local>
2024-11-20 22:55:10 +0200

    Done

--- Cheers.java
+++ Cheers.java
@@ -5,20 +5,31 @@ public class Cheers {
 
             String cheersWord = args[0];
             int repet = Integer.parseInt(args[1]);
+            String letters="aAbBcCdDeEfFgGhHiIjJkKlLnNmMoOpPqQrRsStTuUvVwWxXyYzZ";
+            char ch;
+            int position;
+            String capitalWord = "";
 
             for(int i=0; i<cheersWord.length(); i++){
 
-                if(cheersWord.charAt(i) == 'A' || cheersWord.charAt(i) == 'E'|| cheersWord.charAt(i) == 'F'|| cheersWord.charAt(i) == 'H'|| cheersWord.charAt(i) == 'H'|| cheersWord.charAt(i) == 'I'|| cheersWord.charAt(i) == 'L'|| cheersWord.charAt(i) == 'M'|| cheersWord.charAt(i) == 'N'|| cheersWord.charAt(i) == 'O'|| cheersWord.charAt(i) == 'R'|| cheersWord.charAt(i) == 'S'|| cheersWord.charAt(i) == 'X') {
-                System.out.println("Give me an "+ cheersWord.charAt(i)+": "+cheersWord.charAt(i)+"!");
+                ch = cheersWord.charAt(i);
+                position = letters.indexOf(ch);
+                if(position%2==0) ch=letters.charAt(position+1);
+
+                if(ch == 'A' || ch == 'E'|| ch == 'F'|| ch == 'H'|| ch == 'H'|| ch == 'I'|| ch == 'L'|| ch == 'M'|| ch == 'N'|| ch == 'O'|| ch == 'R'|| ch == 'S'|| ch == 'X') {
+                        System.out.println("Give me an "+ cheersWord.charAt(i)+": "+cheersWord.charAt(i)+"!");
                 }
-                else System.out.println("Give me a  "+ cheersWord.charAt(i)+": "+cheersWord.charAt(i)+"!");
+                else System.out.println("Give me a  "+ ch+": "+ch+"!");
+                capitalWord += ch ; 
             }
             System.out.println("What does that spell?");
 
             int j=0;
             while (j<repet) {
-                System.out.println(cheersWord+"!!!");
+                System.out.println(capitalWord+"!!!");
                 j++;
             }
         }
 }
+
+
