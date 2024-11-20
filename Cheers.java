06a23ec13534884d17aac337552029cb25002ee3
Noam Kraiz <noamkraiz@MacBook-Air-sl-Noam.local>
2024-11-20 19:28:22 +0200

    Done

--- Cheers.java
+++ Cheers.java
@@ -2,5 +2,22 @@
 public class Cheers {
         public static void main(String[] args) {
 	    // Replace this comment with your code
+
+            String cheersWord = args[0];
+            int repet = Integer.parseInt(args[1]);
+
+            for(int i=0; i<cheersWord.length(); i++){
+
+                if(cheersWord.charAt(i) == 'A' || cheersWord.charAt(i) == 'E'|| cheersWord.charAt(i) == 'F'|| cheersWord.charAt(i) == 'H'|| cheersWord.charAt(i) == 'H'|| cheersWord.charAt(i) == 'I'|| cheersWord.charAt(i) == 'L'|| cheersWord.charAt(i) == 'M'|| cheersWord.charAt(i) == 'N'|| cheersWord.charAt(i) == 'O'|| cheersWord.charAt(i) == 'R'|| cheersWord.charAt(i) == 'S'|| cheersWord.charAt(i) == 'X') {
+                System.out.println("Give me an "+ cheersWord.charAt(i)+": "+cheersWord.charAt(i)+"!");
+                }
+                else System.out.println("Give me a "+ cheersWord.charAt(i)+": "+cheersWord.charAt(i)+"!");
+            }
+
+            int j=0;
+            while (j<repet) {
+                System.out.println(cheersWord+"!!!");
+                j++;
+            }
         }
 }
