451ae10bc02f1025babf998909884d3512b8238a
Noam Kraiz <noamkraiz@MacBook-Air-sl-Noam.local>
2024-11-21 18:42:18 +0200

    Done

--- CalcPi.java
+++ CalcPi.java
@@ -15,7 +15,7 @@ public class CalcPi {
 			i++;	
 		}
 
-		System.out.println("pi according to java: "+Math.PI);
+		System.out.println("pi according to Java: "+Math.PI);
 		System.out.println("pi, approximated:     "+sum*4 );
 	}
 }
